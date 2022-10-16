ddac0c947924d696bdb9d2aae42b04a6a99199df
Vladimir_Chernyaev <>
2022-10-16 20:46:14 +0400

    added new info in file Readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array1 = new string[6] {"hello","2","world","Russia","cat","be"};
+string[] array2 = new string[array1.Length];
+FillSecondArray(array1,array2);
+PrintArray(array2);
+void FillSecondArray(string[] array1,string[] array2)
+{
+    int count = 0;
+    for (int i=0; i<array1.Length;i++)
+    {
+        if(array1[i].Length<=3)
+        {
+            array2[count]=array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    Console.WriteLine();
+}
